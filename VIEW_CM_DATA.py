4c8f54e42f7e3134ee9680320bf8b6c219cd71e2
Lucas Costa <lucas.costa@lit.ifce.edu.br>
2018-08-25 10:17:53 -0300

    File to view results of CM variation

--- VIEW_CM_DATA.py
+++ VIEW_CM_DATA.py
@@ -6,7 +6,18 @@ DECIMATION = 1
 NUMBER_OF_ROUNDS = 50
 CM_BIT_MIN = 2
 CM_BIT_MAX = 8
+R = 1
+SHOW = True
+
 oExp = Experiment.load(
     "OBJECTS/EXP_{:02d}/ACC_M{}_{}_CM{}-{}b_ATT{}.txt".format(EXPERIMENT_NUMBER, DECIMATION,
                                                               NUMBER_OF_ROUNDS, CM_BIT_MIN, CM_BIT_MAX, ATT_NUMBER))
 print oExp.show_in_table()
+
+if SHOW:
+    print "-" * 40
+    oDataSetCm = oExp.experimentResults[R - 1]
+    for j, i in enumerate(oDataSetCm.dataSet):
+        print "Rodada ", j + 1
+        print i
+        print "-" * 40
