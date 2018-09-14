3ce23cdbe843ae0657208a3b8593a245e14fc28b
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-09-14 17:44:20 -0300

    Export model in cm View Data

--- VIEW_CM_DATA.py
+++ VIEW_CM_DATA.py
@@ -2,11 +2,11 @@ from MachineLearn.Classes.experiment import Experiment
 
 EXPERIMENT_NUMBER = 4
 ATT_NUMBER = 10
-DECIMATION = 1
+DECIMATION = 14
 NUMBER_OF_ROUNDS = 50
 CM_BIT_MIN = 2
 CM_BIT_MAX = 8
-R = 1
+R = 5
 SHOW = True
 
 oExp = Experiment.load(
@@ -21,3 +21,14 @@ if SHOW:
         print "Rodada ", j + 1
         print i
         print "-" * 40
+oData = oDataSetCm.dataSet[23]
+print oData
+print "Matrix Confusao:"
+print oData.confusion_matrix
+print
+print "Valores para Normalizacao(max, min):"
+print oDataSetCm.normalize_between
+print
+print "Indices das amostras de teste(indexado de 0):"
+print oData.Testing_indexes
+# oData.save_model("MODEL_M14_CM6_TH198_ATT10_ROUND_24.txt")
