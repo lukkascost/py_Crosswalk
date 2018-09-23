f61917f322be959057316e4687afada692606202
Lucas Costa <lucas.costa@lit.ifce.edu.br>
2018-09-23 11:02:15 -0300

    Model to measure time in 24 and 3 att

--- VIEW_CM_DATA.py
+++ VIEW_CM_DATA.py
@@ -1,17 +1,21 @@
 from MachineLearn.Classes.experiment import Experiment
 
 EXPERIMENT_NUMBER = 4
-ATT_NUMBER = 10
+ATT_NUMBER = 3
 DECIMATION = 14
+DECIMATION_MIN = 1
+DECIMATION_MAX = 100
 NUMBER_OF_ROUNDS = 50
 CM_BIT_MIN = 2
 CM_BIT_MAX = 8
+CM_BIT = 8
+
 R = 5
 SHOW = True
 
 oExp = Experiment.load(
-    "OBJECTS/EXP_{:02d}/ACC_M{}_{}_CM{}-{}b_ATT{}.txt".format(EXPERIMENT_NUMBER, DECIMATION,
-                                                              NUMBER_OF_ROUNDS, CM_BIT_MIN, CM_BIT_MAX, ATT_NUMBER))
+    "OBJECTS/EXP_{:02d}/ACC_M{}-{}_{}_CM{}b_ATT{}.txt".format(EXPERIMENT_NUMBER, DECIMATION_MIN,DECIMATION_MAX,
+                                                              NUMBER_OF_ROUNDS, CM_BIT, ATT_NUMBER))
 print oExp.show_in_table()
 
 if SHOW:
@@ -31,4 +35,4 @@ print oDataSetCm.normalize_between
 print
 print "Indices das amostras de teste(indexado de 0):"
 print oData.Testing_indexes
-# oData.save_model("MODEL_M14_CM6_TH198_ATT10_ROUND_24.txt")
+# oData.save_model("MODEL_M14_CM6_TH198_ATT3_ROUND_24.txt")
